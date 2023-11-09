b37961a4dd9268848976634a740d287c9e3947d8
matt <ace@worx.wondermining.net>
2023-11-08 20:21:13 -0600

    add support for recompute docker images

---
+++ chiapos/linux/x86_64/docker-start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ ${CHIA_RECOMPUTE_PROXY} == "true" ]]; then
+  if [[ -n ${CHIA_RECOMPUTE_NODES} ]]; then
+    nodes=""
+    for n in ${CHIA_RECOMPUTE_NODES//,/ }; do
+      nodes+=" -n ${n}"
+    done
+    ./chia_recompute_proxy -p ${CHIA_RECOMPUTE_PORT} ${nodes}
+  else
+    echo "At least one compute server must be specified to run the proxy"
+    exit
+  fi
+else
+  ./chia_recompute_server -p ${CHIA_RECOMPUTE_PORT}
+fi
